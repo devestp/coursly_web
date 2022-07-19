5485b6e211bbc1a42239433ad57b4b151d3f4639
sina <im.devestp@gmail.com>
2022-07-20 02:58:23 +0430

    feat: created course model

---
+++ lib/models/course.dart
@@ -0,0 +1,13 @@
+class Course {
+  final String title;
+  final String type;
+  final int lessonsTotal;
+  final int totalHours;
+
+  Course({
+    required this.title,
+    required this.type,
+    required this.lessonsTotal,
+    required this.totalHours,
+  });
+}
